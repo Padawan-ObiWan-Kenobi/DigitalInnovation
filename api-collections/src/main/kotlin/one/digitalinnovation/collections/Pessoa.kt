d2c4dd2d409969a5af52ee854aea468ade6718bc
Padawan-ObiWan-Kenobi <joaoelias.reis@outlook.com>
2021-10-03 20:27:59 -0300

    atualização para falar sobre os membros da classe

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
@@ -5,6 +5,10 @@ class Pessoa {
     var cpf: String = "123.123.123-12" // Utilizando private como um modificador de visualidade, para que a variavel nao possa ser alterada em tempo de execucao, sendo visivel somente dentro do escopo da classe
     private set // Deixa o get publico, e o set privado
 
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
+
     //Classe interna - quando ao criar a classe principal a variavel nao foi instanciada
     //Criando uma classe dentro de outra classe
     //inner class endereco {
@@ -16,7 +20,9 @@ fun main() {
     val jether = Pessoa()
     //jether.cpf = "4567" // Altera a variavel em tempo de execucao, mesmo que esta variavel ja tenho sido declarado o seu valor anteriormente
 
-    println(jether.nome)
-    println(jether.cpf)
+    //println(jether.nome)
+    //println(jether.cpf)
     //println(jether.endereco().rua)
+
+    println(jether.pessoaInfo())
 }
